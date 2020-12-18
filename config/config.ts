b58e0dab21beaa811e47332e68cf31a5a02d85d5
kiritoxf <yh_txyg@qq.com>
2020-12-18 11:06:14 +0800

    reverse dist

--- config/config.ts
+++ config/config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
   title: false,
   ignoreMomentLocale: true,
   proxy: proxy[REACT_APP_ENV || 'dev'],
-  // publicPath: './',
+  // publicPath: '/DMS2_demo/',
   manifest: {
     basePath: '/',
   },
